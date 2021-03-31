3dd5012012d99e31464c7d54892328a113f9bce4
Nam Le <lethanhnam010490@gmail>
2021-03-31 11:27:24 +0700

    Added user api

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const session = require('express-session');
+
+const app = express();
 // Load config
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config({ path: 'config/config.env'});
 }
 
-const app = express();
+// routes
+const createUsers = require('./routes/api/users');
 
+// connect to db
 const {
   MONGO_USERNAME,
   MONGO_PASSWORD,
@@ -34,6 +39,18 @@ app.use(express.json({ limit: '100mb' }));
 if (process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+
+// creating cookie
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// all route starting with /auth should refer createUsers
+app.use('/auth', createUsers);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
